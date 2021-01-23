d92a388ccf2d1d894b82e8fb0bf9bd0383965908
mewes4 <mewes4@web.de>
2021-01-23 14:01:22 +0100

    database schema initialization

--- src/setup.py
+++ src/setup.py
@@ -6,12 +6,10 @@ import requests
 from bs4 import BeautifulSoup as soup
 import time as t
 
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds=ServiceAccountCredentials.from_json_keyfile_name('secret.json',scope)
-client=gspread.authorize(creds)
+import json
+from .sql import drop_cases, drop_prices, create_cases, create_prices
+import mysql.connector
 
-sheet1=client.open('steam_cases').sheet1
-#sheet2=client.open('casestest').sheet2
 
 item_list=[
     'Chroma',
@@ -42,7 +40,15 @@ item_list=[
 start_url='https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Type%5B%5D=tag_CSGO_Type_WeaponCase&appid=730&q=case+'
 
 
-def initiate(item_list, used_doc):
+def initiate_sheet(item_list, used_doc):
+    #credentials for google api
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('secret.json',scope)
+    client = gspread.authorize(creds)
+
+    sheet1 = client.open('steam_cases').sheet1
+    #sheet2=client.open('casestest').sheet2
+
     print('setting up sheet...')
     i=2
     for search in item_list:
@@ -64,7 +70,30 @@ def initiate(item_list, used_doc):
             print(title)
     print('setup successfull!')
 
-initiate(item_list,sheet1)
+def initiate_db(item_list):
+    #credentials for database
+    with open("secret.json") as f:
+        json_data = json.load(f)
+    database = json_data["database"]
+    host = json_data["host"]
+    username = json_data["username"]
+    password = json_data["password"]
+
+    db = mydb.connector.connect(
+        host=host,
+        user=username,
+        password=password,
+        database=database
+    )
+
+    cursor = db.cursor()
+
+    print("setting up database...")
+    cursor.execute()
+
+
+initiate_db(item_list,sheet1)
+
 #initiate(item_list_old,sheet2)
 
 
